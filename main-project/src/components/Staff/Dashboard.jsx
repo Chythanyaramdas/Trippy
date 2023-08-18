81abd705ff493bb7e26411de07f7dbfd6e0da63d
Chythanyaramdas <cchythanyaramdas@gmail.com>
2023-08-18 20:13:54 +0530

    review dashboard

--- main-project/src/components/Staff/Dashboard.jsx
+++ main-project/src/components/Staff/Dashboard.jsx
@@ -1,11 +1,133 @@
-import React from 'react'
-
-function Dashboard() {
+import React, { useState } from "react";
+import consultationIcon from "../../../assets/adminDashboard/consultationIcon2.webp";
+import userIcon from "../../../assets/adminDashboard/usersIcon.webp";
+import doctorIcon from "../../../assets/adminDashboard/doctorIcon2.webp";
+import ReactApexChart from "react-apexcharts";
+import "./DashBoard.css";
+import Dashboard  from "../../../pages/admin/Dashboard";
+import ReactDatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import moment from "moment";
+function DashBoardComponent({
+  userCount,
+  doctorCount,
+  consultationCount,
+  pieChartOptionsDoctor,
+  pieChartSeriesDoctor,
+  pieChartSeriesAdmin,
+  pieChartOptionsAdmin,
+  linechartOptionsInitial,
+  lineChartSeries,
+  lineChartFrom,
+  lineChartTo,
+  setLineChartFrom,
+  setLineChartTo
+}) {
+  // const [fromDate, setFromDate] = useState(new Date(lineChartFrom));
+  // const [toDate,setToDate] = useState(new Date(lineChartTo))
+  // const handleCalendarClose = () => console.log("Calendar closed");
+  // const handleCalendarOpen = () => console.log("Calendar opened");
+  // console.log(lineChartTo,'lineChartSeries');
   return (
-    <div>
-      
+    <div className="w-full h-auto px-5 py-5 dashboard">
+      <div className="w-fll  grid grid-cols-3 h-44">
+        {/* <div className="bg-slate-500 text-white w-full full"> */}
+        <div className="w-96 h-full mx-auto bg-white shadow-xl shadow-slate-400 flex items-center p-2 rounded-lg ">
+          <div className="w-32 h-32 via-gray-800 rounded-lg overflow-hidden ">
+            <img src={userIcon} alt="" className="w-full h-full object-cover" />
+          </div>
+          <div className="flex flex-col items-center justify-around w-64 py-2 h-full">
+            <h1 className="text-xl font-semibold tracking-wider">
+              Total Users
+            </h1>
+            <p className="text-xl font-semibold tracking-wider">{userCount}</p>
+          </div>
+        </div>
+        {/* </div> */}
+        <div className="w-96 h-full mx-auto bg-white shadow-xl shadow-slate-400 flex items-center p-2 rounded-lg ">
+          <div className="w-32 h-32 via-gray-800 rounded-lg overflow-hidden ">
+            <img
+              src={doctorIcon}
+              alt=""
+              className="w-full h-full object-cover"
+            />
+          </div>
+          <div className="flex flex-col items-center justify-around w-64 py-2 h-full">
+            <h1 className="text-xl font-semibold tracking-wider">
+              Total Doctors
+            </h1>
+            <p className="text-xl font-semibold tracking-wider">
+              {doctorCount}
+            </p>
+          </div>
+        </div>
+        <div className="w-96 h-full mx-auto bg-white shadow-xl shadow-slate-400 flex items-center p-2 rounded-lg ">
+          <div className="w-32 h-32 via-gray-800 rounded-lg overflow-hidden ">
+            <img
+              src={consultationIcon}
+              alt=""
+              className="w-full h-full object-cover"
+            />
+          </div>
+          <div className="flex flex-col items-center justify-around w-64 py-2 h-full">
+            <h1 className="text-xl font-semibold tracking-wider">
+              Total Consultations
+            </h1>
+            <p className="text-xl font-semibold tracking-wider">
+              {consultationCount}
+            </p>
+          </div>
+        </div>
+      </div>
+      <section className="my-10 bg-slate-100 rounded-2xl p-5">
+        <p className="text-lg font-bold ml-4 underline underline-offset-8 text-center my-5">
+          Consultation Wise
+        </p>
+        <div className="w-full h-[50vh]  grid grid-cols-2 items-center  ">
+          <div className=" flex flex-col items-center justify-center bg-white w-[80%] mx-auto h-[90%] rounded-2xl">
+            <ReactApexChart
+              options={pieChartOptionsDoctor}
+              series={pieChartSeriesDoctor}
+              type="pie"
+              width={380}
+            />
+            <p>Doctor</p>
+          </div>
+          <div className="flex flex-col items-center justify-center bg-white w-[80%] mx-auto h-[90%] rounded-2xl">
+            <ReactApexChart
+              options={pieChartOptionsAdmin}
+              series={pieChartSeriesAdmin}
+              type="pie"
+              width={380}
+            />
+            <p>Admin</p>
+          </div>
+        </div>
+      </section>
+      <section className="my-10">
+        <div className="flex">
+    {/* <ReactDatePicker
+      selected={fromDate}
+      onChange={(date) => setFromDate(date)}
+      onCalendarClose={handleCalendarClose}
+      onCalendarOpen={handleCalendarOpen}
+    />
+    <ReactDatePicker
+      selected={toDate}
+      onChange={(date) => setToDate(date)}
+      onCalendarClose={handleCalendarClose}
+      onCalendarOpen={handleCalendarOpen}
+    />` */}
+        </div>
+        <ReactApexChart
+          options={linechartOptionsInitial}
+          series={lineChartSeries}
+          type="line"
+          height={350}
+        />
+      </section>
     </div>
-  )
+  );
 }
 
-export default Dashboard
+export default DashBoardComponent;
---
+++ main-project/src/pages/staff/Dashboards.jsx
@@ -0,0 +1,176 @@
+import React, { useEffect, useState } from "react";
+import AdminNavbar from "../../components/admin/navbar/AdminNavbar";
+import Sidebar from "../../components/admin/sideBar/Sidebar";
+import { AdminApi } from "../../utils/staff/axiosStaff";
+import DashBoardComponent from "../../components/Staff/Dashboard";
+import moment from "moment";
+
+function Dashboard() {
+  const [userCount, setUser] = useState(null);
+  const [doctorCount, setDoctors] = useState(null);
+  const [consultationCount, setConsultationCount] = useState(null);
+  const initialValuesOfDays = ['Sunday','Monday','Tuesday',"Wednesday","Thursday",'Friday','Saturday']
+  const [days,setDays] = useState(initialValuesOfDays)
+  const [dayWiseSales,setDayWiseSales] = useState([])
+  const [lineChartFrom,setLineChartFrom] = useState(moment())
+  const [lineChartTo,setLineChartTo] = useState(moment().subtract(7,'days'))
+  const [lineChartSeries,setLineChartSeries] = useState([{
+    name: "Total Amount",
+    data: []
+}])
+
+
+
+  const pieChartSeriesInitial = [];
+  const pieChartOptionsInitial = {
+    chart: {
+      width: 380,
+      type: "pie",
+    },
+    labels: [],
+    responsive: [
+      {
+        breakpoint: 480,
+        options: {
+          chart: {
+            width: 200,
+          },
+          legend: {
+            position: "bottom",
+          },
+        },
+      },
+    ],
+  };
+
+
+  const linechartOptionsInitial = {
+    chart: {
+      height: 350,
+      type: 'line',
+      zoom: {
+        enabled: false
+      }
+    },
+    dataLabels: {
+      enabled: false
+    },
+    stroke: {
+      curve: 'straight'
+    },
+    title: {
+      text: 'Weekly Profit',
+      align: 'center'
+    },
+    grid: {
+      row: {
+        colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
+        opacity: 0.5
+      },
+    },
+    xaxis: {
+      categories:days ,
+    }
+  }
+
+  const [pieChartSeriesDoctor, setPieChartseriesDoctor] = useState(
+    pieChartSeriesInitial
+  );
+  const [pieChartOptionsDoctor, setPieChartOptionsDoctor] = useState(
+    pieChartOptionsInitial
+  );
+
+  const [pieChartSeriesAdmin,setPieChartSeriesAdmin] = useState(pieChartSeriesInitial)
+  const [pieChartOptionsAdmin,setPieChartOptionsAdmin] = useState(pieChartOptionsInitial)
+
+  const formateDate = (date) => {
+    let result = moment(date).format("L");
+    return result;
+  };
+
+  useEffect(() => {
+    AdminApi
+      .get("/dashBoardChart")
+      .then((res) => {
+        if (res.data.status) {
+          setUser(res.data.userCount);
+          setDoctors(res.data.doctorCount);
+          setConsultationCount(res.data.consultationCount);
+          res.data.pieChartData.forEach((data) => {
+            setPieChartseriesDoctor((pre) => [...pre, data["doctorPayment"]]);
+            setPieChartSeriesAdmin((pre)=> [...pre,data['adminPayment']])
+          });
+
+          res.data.pieChartData.forEach((data) => {
+            setPieChartOptionsDoctor((prev) => ({
+              ...prev,
+              labels: [...prev.labels, data._id], // Corrected syntax here
+            }));
+
+            setPieChartOptionsAdmin((prev)=>(
+              {
+                ...prev,
+                labels:[...prev.labels,data._id]
+              }
+            ))
+
+          });
+          let weeklySalesReport = res.data.weeklySalesReport
+          
+          let newDaysArray = []
+          let newArray =[]
+          for(let i = 0;i< weeklySalesReport.length;i++){
+             newArray.push(weeklySalesReport[i]?.totalAmount) 
+            // else newArray.push(null)
+            console.log(i);
+          }
+
+          weeklySalesReport.forEach((report)=>{
+            console.log('report',report._id);
+            let formattedDate = formateDate(report._id)
+            console.log(formattedDate);
+            newDaysArray.push(formattedDate)
+          })
+          setDays([...newDaysArray])
+          console.log('---newArray-----');
+          setLineChartSeries(prev=>{
+            return [{...prev[0],['data'] : newArray} ]
+          })
+          setLineChartFrom(res.data.lineChartFrom)
+          setLineChartTo(res.data.lineChartTo)
+        }
+      })
+      .catch(()=>{});
+  }, []);
+  // console.log(pieChartSeriesDoctor,'pieChartSeriesDoctorrrrr');
+  // console.log(pieChartOptionsDoctor,'pieChartOptionsDoctor');
+  return (
+    <div className='sticky top-0 z-10 h-[12vh] '>
+      <AdminNavbar />
+      <div className="grid grid-cols-[1fr_7fr] md:grid-cols-[1.5fr_8.5fr] w-full sticky top-[12vh]">
+        <Sidebar />
+        <div className="w-full h-[88vh]  overflow-y-scroll">
+        <DashBoardComponent
+          userCount={userCount}
+          doctorCount={doctorCount}
+          consultationCount={consultationCount}
+          pieChartSeriesDoctor={pieChartSeriesDoctor}
+          pieChartOptionsDoctor={pieChartOptionsDoctor}
+          pieChartSeriesAdmin={pieChartSeriesAdmin}
+          pieChartOptionsAdmin={pieChartOptionsAdmin}
+          lineChartSeries={lineChartSeries}
+          linechartOptionsInitial={linechartOptionsInitial}
+          lineChartFrom={lineChartFrom}
+          lineCharto={lineChartTo}
+          setLineChartFrom={setLineChartFrom}
+          setLineChartTo={setLineChartTo}
+        />
+        </div>
+        
+      </div>
+      
+    </div>
+  );
+}
+
+export default Dashboard;
